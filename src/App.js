2c25564707e65c291cd448f9fae0d334f81a32bd
rainbow5079 <rlaxogh507906@gmail.com>
2024-06-17 07:13:56 +0900

    :zap: Swiper 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import NavBar from "./components/NavBar";
+import Intro from "./pages/Intro";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Scrollbar, Mousewheel, Keyboard } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/scrollbar";
+import "swiper/css/mousewheel";
+import "swiper/css/keyboard";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Swiper
+        modules={[Scrollbar, Mousewheel, Keyboard]}
+        scrollbar={{ drawable: true, el: "" }}
+        direction={"vertical"}
+        mousewheel={true}
+        keyboard={true}
+        style={{ height: "93vh" }}
+      >
+        <SwiperSlide>
+          <Intro />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Intro />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
