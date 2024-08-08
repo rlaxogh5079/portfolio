18837a03dc4a243a89a876009ae9dc4db21a8b41
rainbow5079 <rlaxogh507906@gmail.com>
2024-08-08 18:16:23 +0900

    :lipstick: Home Page 완성

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import "../styles/Home.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <header className="home-header">
+        <span>DEVELOPER</span>
+        <span>PORTFOLIO</span>
+        <span>Made By 2024</span>
+      </header>
+      <div className="home-title1">Hello</div>
+      <div className="home-title2">Developer</div>
+      <div className="about-me">
+        MY NAME IS <br /> Tae Ho Kim
+      </div>
+      <div className="underline"></div>
+    </div>
+  );
+};
+
+export default Home;
