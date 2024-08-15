9b341fb15445b365a805393005fa798b44a13c60
rlaxogh5079 <rlaxogh507906@gmail.com>
2024-08-16 02:17:55 +0900

    :zap: imporving animation performance

--- src/pages/Career.js
+++ src/pages/Career.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 const Career = () => {
   const [flag, setFlag] = useState([true, false, false, false]);
+  const [moving, setMoving] = useState(false);
   return (
     <div className="career-container">
       <div className="career-title">Career</div>
@@ -11,9 +12,13 @@ const Career = () => {
       </div>
       <ul className="career-year-list">
         <li
-          className={`${flag[0] ? "active" : ""}`}
+          className={`${flag[0] ? "active" : ""} `}
           onClick={() => {
             setFlag([true, false, false, false]);
+            setMoving(true);
+            setTimeout(() => {
+              setMoving(false);
+            }, 500);
           }}
         >
           2021
@@ -22,6 +27,10 @@ const Career = () => {
           className={`${flag[1] ? "active" : ""}`}
           onClick={() => {
             setFlag([false, true, false, false]);
+            setMoving(true);
+            setTimeout(() => {
+              setMoving(false);
+            }, 500);
           }}
         >
           2022
@@ -30,6 +39,10 @@ const Career = () => {
           className={`${flag[2] ? "active" : ""}`}
           onClick={() => {
             setFlag([false, false, true, false]);
+            setMoving(true);
+            setTimeout(() => {
+              setMoving(false);
+            }, 500);
           }}
         >
           2023
@@ -38,11 +51,15 @@ const Career = () => {
           className={`${flag[3] ? "active" : ""}`}
           onClick={() => {
             setFlag([false, false, false, true]);
+            setMoving(true);
+            setTimeout(() => {
+              setMoving(false);
+            }, 500);
           }}
         >
           2024
         </li>
-        <div className="year-background"></div>
+        <div className={`year-background ${moving ? "moving" : ""}`}></div>
       </ul>
     </div>
   );
