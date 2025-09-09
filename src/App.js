1b82d40e8dc00bfeedbc1230c450363060b87b2f
rlaxogh5079 <rlaxogh507906@gmail.com>
2025-09-10 01:07:47 +0900

    :lipstick: Swiper 페이지 전환 제공

--- src/App.js
+++ src/App.js
@@ -6,16 +6,59 @@ import Experience from "./pages/Experience";
 import Contact from "./pages/Contact";
 import "./styles/App.scss";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Mousewheel,
+  Keyboard,
+  Scrollbar,
+  EffectCreative,
+} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-creative";
+import "swiper/css/scrollbar";
+
 const App = () => {
   return (
-    <>
-      <Intro />
-      <AboutMe />
-      <Projects />
-      <Skills />
-      <Experience />
-      <Contact />
-    </>
+    <Swiper
+      className="app-swiper"
+      direction="vertical"
+      modules={[Mousewheel, Keyboard, Scrollbar, EffectCreative]}
+      speed={700}
+      mousewheel={{
+        enabled: true,
+      }}
+      keyboard={{ enabled: true }}
+      scrollbar={{ draggable: true }}
+      effect={"creative"}
+      creativeEffect={{
+        prev: {
+          shadow: true,
+          translate: [0, "-100%", 0],
+        },
+        next: {
+          translate: [0, "100%", 0],
+        },
+      }}
+    >
+      <SwiperSlide>
+        <Intro />
+      </SwiperSlide>
+      <SwiperSlide>
+        <AboutMe />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Projects />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Skills />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Experience />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Contact />
+      </SwiperSlide>
+    </Swiper>
   );
 };
 
